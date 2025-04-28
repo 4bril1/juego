c9ffdec39f8801ae47b78f1f2db533d213fbf1c6
4bril1 <fernandacasallas1222@gmail.com>
2025-04-28 11:51:11 -0500

    empezando movimientos

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
 from personaje import personaje
+import constantes
 player = personaje(50, 50)
 pygame.init()
-ancho=800
-alto=600
+ancho=constantes.ANCHO_VENTANA
+alto=constantes.ALTO_VENTANA
 ventana = pygame.display.set_mode((ancho, alto))
 run = True
 while run:
@@ -11,5 +12,15 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run= False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                print("Izquierda")
+            if event.key == pygame.K_d:
+                print("Derecha")
+            if event.key == pygame.K_w:
+                print("Arriba")
+            if event.key == pygame.K_s:
+                print("Abajo")
+
     pygame.display.update()
 pygame.quit()
